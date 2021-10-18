c0a5986ef420ed2971a8ff18dc25f797858e359a
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2021-10-18 06:30:26 +0300

    This will add the signup form on the homepage

--- cloneapp/views.py
+++ cloneapp/views.py
@@ -6,7 +6,16 @@ from django.contrib.auth import login
 from .forms import SignUpForm
 
 def landing(request):
-    return render(request, 'home.html')
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            login(request, user)
+            messages.success(request, "Registration successfull")
+            return redirect("cloneapp:index.html")
+        messages.error(request, "Unsuccessful registration. Invalid Information")
+    form = SignUpForm()
+    return render(request, 'home.html', context={"signup_form":form})
 
 
 def signup(request):
